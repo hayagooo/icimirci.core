6a203b8372c61b017a2dfd408d7f00923149058d
081332620512 <febrimozak@gmail.com>
2021-09-03 21:43:19 +0700

    ApiCategory

---
+++ app/Http/Controllers/Admin/Product/ApiCategoryController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category_product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+
+class ApiCategoryController extends Controller
+{
+    public $data;
+    public $dataType;
+
+    public function __construct(Category_product $data){
+        $this->data = $data;
+        $this->dataType = 'Category_product';
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'category' => ['required', 'max:20']
+        ]);
+        $data = new Category_product();
+        $data->category = $request->category;
+        $data->save();
+        return $this->onSuccess($this->dataType, $data, 'created');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $data = Category_product::find($id);
+        $data->category = $request->category;
+        $data->save();
+        return $this->onSuccess($this->dataType, $data, 'updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $data = Category_product::find($id);
+        $data->delete();
+        return $this->onSuccess($this->dataType, $data, 'Destroyed');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Product\ApiProductController;
+use App\Http\Controllers\Admin\Product\ApiCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('product', ApiProductController::class);
+Route::resource('category', ApiCategoryController::class);
 
